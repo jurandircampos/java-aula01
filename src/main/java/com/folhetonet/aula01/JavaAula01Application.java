847518ea8f83408d3beb8d25a1c4915afb374f37
jurandircampos <jurandir.git@gmail.com>
2018-08-02 16:07:43 -0300

    Versão inicial STS - Aula 01

---
+++ src/main/java/com/folhetonet/aula01/JavaAula01Application.java
@@ -0,0 +1,12 @@
+package com.folhetonet.aula01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAula01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAula01Application.class, args);
+	}
+}
---
+++ src/test/java/com/folhetonet/aula01/JavaAula01ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.folhetonet.aula01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaAula01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
